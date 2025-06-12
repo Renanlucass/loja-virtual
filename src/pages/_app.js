2b5e80cfa5698fecadcc8e36c7782c68992ee1d1
Renanlucass <renanlucas0014@gmail.com>
2025-06-12 13:55:42 -0300

    Criando funcionalidade do carrinho de compras

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,10 +1,14 @@
 import "@/styles/globals.css";
 import Layout from '../components/Layout';
 import { CartProvider } from "@/context/CartContext";
+import Head from 'next/head';
 
 export default function App({ Component, pageProps }) {
   return (
     <CartProvider>
+      <Head>
+        <link rel="icon" href="/logo-maria.png" />
+      </Head>
       <Layout>
         <Component {...pageProps} />
       </Layout>
